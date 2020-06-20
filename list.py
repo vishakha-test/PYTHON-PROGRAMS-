b5955577bb3477cb5c9d04b1aaefc1160fca7368
ANSHUL12-FIX <64637596+ANSHUL12-FIX@users.noreply.github.com>
2020-06-20 21:33:29 +0530

    list.py

---
+++ list.py
@@ -0,0 +1,11 @@
+List = [12,-7,5,64,-14]
+for number in List:
+    if number >= 0:
+        print(number, end = " ")
+print("\n")
+List1 = [12,14,-95,3]
+for number in List1:
+    if number >= 0:
+        print(number, end = " ")
+        
+
